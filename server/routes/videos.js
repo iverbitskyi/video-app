06d6ce9f9fb1910c33ef8e2397864e42e5c0159f
v_verbitskyi <ivan.verbitskyj@gmail.com>
2022-09-04 05:46:48 +0300

    server is completed

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -6,9 +6,9 @@ const router = express.Router();
 
 //create a video
 router.post("/", verifyToken, addVideo);
-router.put("/:id", verifyToken, addVideo);
-router.delete("/:id", verifyToken, addVideo);
-router.get("/find/:id", addVideo);
+router.put("/:id", verifyToken, updateVideo);
+router.delete("/:id", verifyToken, deleteVideo);
+router.get("/find/:id", getVideo);
 router.put("/view/:id", addView);
 router.get("/trend", trend);
 router.get("/random", random);
